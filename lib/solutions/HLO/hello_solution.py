fde8309f12dbc4aae9661ef550c564e282d9507b
sbx_user1051 <sbx_user1051@169.254.18.107>
2025-06-30 20:35:41 +0000

    Mon Jun 30 20:35:41 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,12 @@
 class HelloSolution:
     
     # friend_name = unicode string
-    def hello(self, friend_name):
-        raise NotImplementedError()
+    def hello(self, friend_name:str)->str:
+        #raise NotImplementedError()
+        """
+        Returns a greeting message for the given name.
+
+        :param name: A string containing a name
+        :return: A string containing a message
+        """
+        return f"Hello, {friend_name}!"
