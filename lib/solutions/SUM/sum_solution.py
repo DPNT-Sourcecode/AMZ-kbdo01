8137db771afa1c734347835e21d24af5b04f4744
sbx_user1051 <sbx_user1051@169.254.18.107>
2025-06-30 20:30:01 +0000

    Mon Jun 30 20:30:01 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,20 @@
 
 class SumSolution:
     
-    def compute(self, x, y):
-        raise NotImplementedError()
+    def sumSolution(a: int, b: int) -> int:
+        """
+        Sums two positive integers between 0 and 100 (inclusive).
+
+        :param a: First integer (0-100)
+        :param b: Second integer (0-100)
+        :return: Sum of a and b
+        :raises ValueError: If a or b is not between 0 and 100
+        """
+        if not (0 <= a <= 100) or not (0 <= b <= 100):
+            raise ValueError("Both numbers must be between 0 and 100 inclusive.")
+        return a + b
+
+
+    #def compute(self, x, y):
+    #    raise NotImplementedError()
+
