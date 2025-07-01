8b29b38339fcb0553b325058c86e1144cd5e13e3
sbx_user1051 <sbx_user1051@169.254.2.225>
2025-07-01 21:48:53 +0000

    Tue Jul 01 21:48:53 UTC 2025

--- lib/solutions/AMZ/amazing_solution.py
+++ lib/solutions/AMZ/amazing_solution.py
@@ -24,14 +24,19 @@ class AmazingSolution:
             if "LEGACY_RANDOM_MAGIC_NUMBER" in maze_generation_options and maze_generation_options["LEGACY_RANDOM_MAGIC_NUMBER"] is not None:
                 magic_number = str(maze_generation_options["LEGACY_RANDOM_MAGIC_NUMBER"])
             if "DEAD_END_ON_LAST_ROW_BEHAVIOUR" in maze_generation_options and maze_generation_options["DEAD_END_ON_LAST_ROW_BEHAVIOUR"] is not None:
-                dead_end_behaviour = str(maze_generation_options["DEAD_END_ON_LAST_ROW_BEHAVIOUR"])
+                dead_end_behaviour_str = str(maze_generation_options["DEAD_END_ON_LAST_ROW_BEHAVIOUR"])
 
         # Add ENTRY_COLUMN (or blank line)
         input_data += f"{entry_column}\n"
         # Add LEGACY_RANDOM_MAGIC_NUMBER (or blank line)
         input_data += f"{magic_number}\n"
         # Add DEAD_END_ON_LAST_ROW_BEHAVIOUR (or blank line)
-        input_data += f"{dead_end_behaviour}\n"
+        if dead_end_behaviour_str == "CREATE_TREASURE":
+            input_data += f"1\n"
+        elif dead_end_behaviour_str == "CREATE_EXIT":
+            input_data += f"0\n"
+        else:
+            input_data += f"\n"
 
         result = subprocess.run(
             [sys.executable, script_path],
@@ -56,6 +61,7 @@ class AmazingSolution:
 
 if __name__ == "__main__":
     # Example usage with DEAD_END_ON_LAST_ROW_BEHAVIOUR as a map key
-    #result = AmazingSolution().amazing_maze(2, 2, { "LEGACY_RANDOM_MAGIC_NUMBER": "0.5", "DEAD_END_ON_LAST_ROW_BEHAVIOUR": "CREATE_TREASURE" })  # Example
-    result = AmazingSolution().amazing_maze(2, 2, { "LEGACY_RANDOM_MAGIC_NUMBER": "0.5" })  # Example
+    #result = AmazingSolution().amazing_maze(3, 4, { "ENTRY_COLUMN": "1", "DEAD_END_ON_LAST_ROW_BEHAVIOUR": "CREATE_TREASURE" })  # Example
+    result = AmazingSolution().amazing_maze(3, 4, { "ENTRY_COLUMN": "1", "DEAD_END_ON_LAST_ROW_BEHAVIOUR": "CREATE_EXIT" })  # Example
+    #result = AmazingSolution().amazing_maze(2, 2, { "LEGACY_RANDOM_MAGIC_NUMBER": "0.5" })  # Example
     print(result)
